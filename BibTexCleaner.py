6081ec6773a8812ee0c375fb03a7ab389e00ad59
Ryan <rchiechi@gmail.com>
2017-08-08 08:33:48 +0200

    Added argparse and functions

--- BibTexCleaner.py
+++ BibTexCleaner.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-import sys,os,tempfile,shutil
+import sys,os,tempfile,shutil,argparse,pickle
 
 try:
     import pip
@@ -28,59 +28,92 @@ from colorama import init,Fore,Back,Style
 
 # Setup colors
 init(autoreset=True)
-YELLOW=Fore.YELLOW
-WHITE=Fore.WHITE
-RED=Fore.RED
-TEAL=Fore.CYAN
-GREEN=Fore.GREEN
-BLUE=Fore.BLUE
-RS=Style.RESET_ALL
+
+# Parse args
+desc = 'Cleanup a bibtex file before submission.'
+
+parser = argparse.ArgumentParser(description=desc,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('infile', type=str, nargs=1, default=[], 
+    help='Bibtex file to parse.')
+parser.add_argument('-r','--refresh', action='store_true', default=False,
+    help="Refresh cached journal list.")
+parser.add_argument('-d','--database', type=str, 
+    default='https://raw.githubusercontent.com/JabRef/reference-abbreviations/master/journals/journal_abbreviations_general.txt',
+    help="Databse of journal abbreviations.")
+parser.add_argument('-c','--custom', action='append', default=[],
+        help="Cust abbreviations separated by equal signs, e.g., -c 'Journal of Kittens=J. Kitt.'\
+        You can call this argument more than once. These will be cached.")
 
 
+opts=parser.parse_args()
+#CACHEDIR = os.path.join(os.path.expanduser('~'),os.argv[0]+".cache")
 CACHEDIR = tempfile.gettempdir()
 
-#TODO actually parse the command line
-if len(sys.argv) < 2:
-    print('%sI need a bib file to parse!' % RED)
+if not opts.infile:
+    print('%sI need a bib file to parse!' % Fore.RED)
     sys.exit()
-elif not os.path.exists(sys.argv[1]):
-    print('%s%s does not exist!' % (RED,sys.argv[1]))
+elif not os.path.exists(opts.infile[0]):
+    print('%s%s does not exist!' % (Fore.RED,opts.infile[0]))
 
-BIBFILE=os.path.abspath(sys.argv[1])
+BIBFILE=os.path.abspath(opts.infile[0])
 # Make a backup copy
-print('%sBacking %s up to %s' % (YELLOW,os.path.basename(BIBFILE),os.path.basename(BIBFILE)+'.bak'))
-shutil.copy(BIBFILE,BIBFILE+'.bak')
+print('%sBacking %s up to %s' % (Fore.YELLOW,os.path.basename(BIBFILE),os.path.basename(BIBFILE)+'.bak'))
+shutil.copy2(BIBFILE,BIBFILE+'.bak')
 
 # Use a cache file so we do not have to fetch the abbreviations on each run
-JCACHE=os.path.join(CACHEDIR,'journal_abbreviations_general.txt')
-jstr = ''
+JCACHE=os.path.join(CACHEDIR,'journal_abbreviations.cache')
 
-if not os.path.exists(JCACHE):
-    print('%sFetching list of common journal abbreviations.' % YELLOW)
+if os.path.exists(JCACHE) and opts.refresh:
+    os.remove(JCACHE)
+
+def getCache():
+    journals = {}
+    if not os.path.exists(JCACHE):
+        print('%sFetching list of common journal abbreviations.' % Fore.YELLOW)
+        try:
+            r = requests.get('https://raw.githubusercontent.com/JabRef/reference-abbreviations/master/journals/journal_abbreviations_general.txt')
+            journals = __parseabbreviations(r.text.split('\n'))
+            #journals = __parseabbreviations(str(r.content,encoding='utf-8'))
+        except Exception as msg:
+            print("%sError fetching journal abbreviations: %s" % (Fore.RED,str(msg)) )
+    else:
+        try:
+            journals = pickle.load(open(JCACHE,'rb'))
+            print('%sRead journal abbreciations from %s.' % (Fore.YELLOW,JCACHE))
+        except:
+            print('%sError loading cache from %s.' % (Fore.RED,JCACHE))
+            sys.exit()
+    return journals
+
+def putCache(journals):
     try:
-        r = requests.get('https://raw.githubusercontent.com/JabRef/reference-abbreviations/master/journals/journal_abbreviations_general.txt')
-        jstr = str(r.content,encoding='utf-8')
-        with open(JCACHE,'wb') as fh:
-            fh.write(r.content)
-    except Exception as msg:
-        print("%sError fetching journal abbreviations: %s" % (RED,str(msg)) )
-else:
-    print('%sReading journal abbreciations from %s.' % (YELLOW,JCACHE))
-    with open(JCACHE,'rt') as fh:
-        jstr = str(fh.read())
+        pickle.dump(journals,open(JCACHE,'wb'))
+        print('%sSaved cache to %s' % (Fore.YELLOW,JCACHE))
+    except:
+        print('%sError saving cache to %s' % (Fore.RED,JCACHE))
+
+def __parseabbreviations(jlines):
+    journals = {}
+    for l in jlines:
+        try:
+            t,a = l.split('=')
+        except ValueError as msg:
+            continue
+        journals[t.strip()] = a.strip()
+        if len(t.split('(')) > 1:
+            journals[t.split('(')[0].strip()] = a.split('(')[0].strip()
+    return journals
 
 # Parse journal abbreviations
-journals = {}
-for l in jstr.split('\n'):
-    try:
-        t,a = l.split('=')
-    except ValueError as msg:
-        continue
-    journals[t.strip()] = a.strip()
-    if len(t.split('(')) > 1:
-        journals[t.split('(')[0].strip()] = a.split('(')[0].strip()
+journals = getCache()
+# Parse custom abbreviations
+journals.update(__parseabbreviations(opts.custom))
+# Update cache
+putCache(journals)
 
-print("%sRead %s journals." % (BLUE,len(journals.keys())) )
+
+print("%sRead %s journals." % (Fore.BLUE,len(journals.keys())) )
 
 # Setup BibTex Parser
 parser = BibTexParser()
@@ -88,7 +121,7 @@ parser.customization = convert_to_unicode
 with open(BIBFILE) as fh:
     bib_database = bibtexparser.load(fh, parser=parser)
 
-print('%s # # # # %s\n' % (Style.BRIGHT,RS) )
+print('%s # # # # %s\n' % (Style.BRIGHT,Style.RESET_ALL) )
 
 # Counters
 n = 0
@@ -107,7 +140,8 @@ for bib in bib_database.entries:
             c += 1
             clean[-1]['title'] = cleaned
         if bib['journal'] in journals and bib['journal'] != journals[bib['journal']]:
-            print('%s%s%s%s -> %s%s%s' % (Style.BRIGHT,TEAL,bib['journal'],WHITE,TEAL,journals[bib['journal']],RS))
+            print('%s%s%s%s -> %s%s%s' % (Style.BRIGHT,Fore.CYAN,bib['journal'],
+                Fore.WHITE,Fore.CYAN,journals[bib['journal']],Style.RESET_ALL))
             clean[-1]['journal'] = journals[bib['journal']]
         try:
             _p = clean[-1]['pages'].split('-')[0]
@@ -115,11 +149,10 @@ for bib in bib_database.entries:
             _p = clean[-1]['pages']
         _j, _v, _c = clean[-1]['journal'],clean[-1]['volume'],clean[-1]['ID']
         dedupe.append( (_p, _v, _j, _c) )
-
     except KeyError:
         if bib['ENTRYTYPE'] == 'journal':
             f.append(bib)
-print('\n%s # # # # %s' % (Style.BRIGHT,RS) )
+print('\n%s # # # # %s' % (Style.BRIGHT,Style.RESET_ALL) )
 
 dupes = []
 # De-dupe check
@@ -136,7 +169,8 @@ writer = BibTexWriter()
 # Overwrite original BibTex file
 with open(BIBFILE, 'w') as bibfile:
         bibfile.write(writer.write(bib_database))
-print('%s%sParsed: %s\n%sCleaned: %s\n%sDupes: %s\n%sFailed:%s%s' % (Style.BRIGHT,GREEN,n,YELLOW,c,TEAL,len(dupes),RED,len(f),RS))
+print('%s%sParsed: %s\n%sCleaned: %s\n%sDupes: %s\n%sFailed:%s%s' % \
+        (Style.BRIGHT,Fore.GREEN,n,Fore.YELLOW,c,Fore.CYAN,len(dupes),Fore.RED,len(f),Style.RESET_ALL))
 if len(f):
     print('\nEntries that produced errors:\n')
     print(f)
@@ -147,7 +181,7 @@ if len(dupes):
         print('\t\t# # #')
         a = bib_database.entries_dict[d[0][3]]
         b = bib_database.entries_dict[d[1][3]]
-        print('   %s%s%s\t%s' % (Style.BRIGHT,TEAL,a['ID'],b['ID']))
-        print('%sJournal: %s%s%s\t%s' %(YELLOW,Style.BRIGHT,WHITE,a['journal'],b['journal']))
-        print('%sVolume: %s%s%s\t\t%s' %(YELLOW,Style.BRIGHT,WHITE,a['volume'],b['volume']))
-        print('%sPages: %s%s%s\t%s' %(YELLOW,Style.BRIGHT,WHITE,a['pages'],b['pages']))
+        print('   %s%s%s\t%s' % (Style.BRIGHT,Fore.CYAN,a['ID'],b['ID']))
+        print('%sJournal: %s%s%s\t%s' %(Fore.YELLOW,Style.BRIGHT,Fore.WHITE,a['journal'],b['journal']))
+        print('%sVolume: %s%s%s\t\t%s' %(Fore.YELLOW,Style.BRIGHT,Fore.WHITE,a['volume'],b['volume']))
+        print('%sPages: %s%s%s\t%s' %(Fore.YELLOW,Style.BRIGHT,Fore.WHITE,a['pages'],b['pages']))
