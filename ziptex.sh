849bc8c9e749877afa8cc34ec064e7bc216f6401
Ryan Chiechi <ryan@rcclab.com>
2020-01-29 09:24:35 +0100

    Beautified

--- ziptex.sh
+++ ziptex.sh
@@ -34,93 +34,93 @@ RS=$(tput sgr0 2>/dev/null)
 #
 
 usage() {
-	echo "${LIME_YELLOW}Usage: $0 [ -f] [ -z ] [ -j ] [ -o OUTDIR ] .tex <.tex> ... ${RS}"
+  echo "${LIME_YELLOW}Usage: $0 [ -f] [ -z ] [ -j ] [ -o OUTDIR ] .tex <.tex> ... ${RS}"
 }
 
 exit_abnormal() {
-    usage
-    exit 1
+  usage
+  exit 1
 }
 
 finddeps() {
-	pdflatex -draft -record -halt-on-error "$1" >/dev/null
-	awk '!x[$0]++' ${1%.tex}.fls | sed '/^INPUT \/.*/d' | sed '/^OUTPUT .*/d' | sed '/^PWD .*/d' | sed 's/^INPUT //g'
+  pdflatex -draft -record -halt-on-error "$1" >/dev/null
+  awk '!x[$0]++' ${1%.tex}.fls | sed '/^INPUT \/.*/d' | sed '/^OUTPUT .*/d' | sed '/^PWD .*/d' | sed 's/^INPUT //g'
 }
 
 checktex() { # This function should only be called from $TMPDIR
   local __texok=0
-	for TEX in ${TEXFILES[@]}; do
-		pdflatex -draft -halt-on-error "${TEX}" >/dev/null
-		if [[ $? == 0 ]]; then
-			echo "${GREEN}${TEX} is OK.${RS}"
-		else
-			echo "${RED}${TEX} is NOT OK.${RS}"
-			__texok=1
-		fi
+  for TEX in ${TEXFILES[@]}; do
+    pdflatex -draft -halt-on-error "${TEX}" >/dev/null
+    if [[ $? == 0 ]]; then
+      echo "${GREEN}${TEX} is OK.${RS}"
+    else
+      echo "${RED}${TEX} is NOT OK.${RS}"
+      __texok=1
+    fi
   done
-	return $__texok
+  return $__texok
 }
 
 catclass() { # This function should only be called from $TMPDIR
-	# Check for CUSTOM_CLASS
-	[[ -z ${CUSTOM_CLASS} ]] && return
+  # Check for CUSTOM_CLASS
+  [[ -z ${CUSTOM_CLASS} ]] && return
   # If a copy of the cls file exists locally, use that one
-	[[ -f $(basename ${CUSTOM_CLASS}) ]] && classfile=$(basename ${CUSTOM_CLASS}) || classfile=${CUSTOM_CLASS}
-	for TEX in ${TEXFILES[@]}; do
-		grep '\\documentclass' "${TEX}" | grep -q $(basename ${classfile} | sed 's/\.cls//g')
-		if [[ $? == 0 ]]; then
-			echo "${LIME_YELLOW}Concatenating $(basename ${classfile}) into ${TEX} for portability.${RS}"
-			mv "${TEX}" "${TEX}.orig"
-			printf "\\\begin{filecontents}{$(basename ${classfile})}\n" > "${TEX}"
-			cat "${classfile}" >> "${TEX}"
-			printf "\\\end{filecontents}\n" >> "${TEX}"
-			cat "${TEX}.orig" >> "${TEX}"
-			rm "${TEX}.orig"
-			[[ "$classfile" != "${CUSTOM_CLASS}" ]] && echo "$classfile" >> .todel
-		fi
-	done
+  [[ -f $(basename ${CUSTOM_CLASS}) ]] && classfile=$(basename ${CUSTOM_CLASS}) || classfile=${CUSTOM_CLASS}
+  for TEX in ${TEXFILES[@]}; do
+    grep '\\documentclass' "${TEX}" | grep -q $(basename ${classfile} | sed 's/\.cls//g')
+    if [[ $? == 0 ]]; then
+      echo "${LIME_YELLOW}Concatenating $(basename ${classfile}) into ${TEX} for portability.${RS}"
+      mv "${TEX}" "${TEX}.orig"
+      printf "\\\begin{filecontents}{$(basename ${classfile})}\n" > "${TEX}"
+      cat "${classfile}" >> "${TEX}"
+      printf "\\\end{filecontents}\n" >> "${TEX}"
+      cat "${TEX}.orig" >> "${TEX}"
+      rm "${TEX}.orig"
+      [[ "$classfile" != "${CUSTOM_CLASS}" ]] && echo "$classfile" >> .todel
+    fi
+  done
 }
 
 cataux() { # This function should only be called from $TMPDIR
-	for TEX in ${TEXFILES[@]}; do
-		ls *.aux | while read aux; do
-			#grep -q "${aux%.*}" ${TEX}
-			egrep -q ".*?\{\s*${aux%.*}\s*\}.*?" "${TEX}"
-			if [[ $? == 0 ]]; then
-				echo "${LIME_YELLOW}Concatenating $(basename ${aux}) into ${TEX} files for portability.${RS}"
-				mv "${TEX}" "${TEX}.orig"
-				printf "\\\begin{filecontents}{$(basename ${aux})}\n" > "${TEX}"
-				cat "${aux}" >> "${TEX}"
-				printf "\\\end{filecontents}\n" >> "${TEX}"
-				cat "${TEX}.orig" >> "${TEX}"
-				rm "${TEX}.orig"
-				echo "${aux}" >> .todel
-			fi
-		done
-	done
+  for TEX in ${TEXFILES[@]}; do
+    ls *.aux | while read aux; do
+      #grep -q "${aux%.*}" ${TEX}
+      egrep -q ".*?\{\s*${aux%.*}\s*\}.*?" "${TEX}"
+      if [[ $? == 0 ]]; then
+        echo "${LIME_YELLOW}Concatenating $(basename ${aux}) into ${TEX} files for portability.${RS}"
+        mv "${TEX}" "${TEX}.orig"
+        printf "\\\begin{filecontents}{$(basename ${aux})}\n" > "${TEX}"
+        cat "${aux}" >> "${TEX}"
+        printf "\\\end{filecontents}\n" >> "${TEX}"
+        cat "${TEX}.orig" >> "${TEX}"
+        rm "${TEX}.orig"
+        echo "${aux}" >> .todel
+      fi
+    done
+  done
 }
 
 flattendirs() { # This function should only be called from $TMPDIR
-	for TEX in ${TEXFILES[@]}; do
-		echo "${LIME_YELLOW}Flattening directory structure for ${TEX}.${RS}"
-		_gfxpath=$(grep graphicspath ${TEX}| cut -d '{' -f '2-' | tr -d '{}/')
-		if [[ ! -d "${_gfxpath}" ]]; then
-			ls
-			echo "${RED} Warning: graphicspath \"${_gfxpath}\" not found (could be a case-sensitivity issue!"
-		else
-			find "$_gfxpath" \
-						-depth -type f -exec mv -v "{}" ./ \;
-		fi
-		sed -i.bak '/\\graphicspath.*/d' "${TEX}"
-	done
+  for TEX in ${TEXFILES[@]}; do
+    echo "${LIME_YELLOW}Flattening directory structure for ${TEX}.${RS}"
+    _gfxpath=$(grep graphicspath ${TEX}| cut -d '{' -f '2-' | tr -d '{}/')
+    if [[ ! -d "${_gfxpath}" ]]; then
+      ls
+      echo "${RED} Warning: graphicspath \"${_gfxpath}\" not found (could be a case-sensitivity issue!"
+    else
+      find "$_gfxpath" \
+        -depth -type f -exec mv -v "{}" ./ \;
+    fi
+    sed -i.bak '/\\graphicspath.*/d' "${TEX}"
+  done
   find ./ -depth -type d -empty -delete
 }
 
 
 
 if [ ! -n "$1" ]; then
-    usage
-    exit 0
+  usage
+  exit 0
 fi
 
 ZIP=0
@@ -128,47 +128,47 @@ BZ=0
 FORCE=0
 
 while getopts ":o:zjf" options; do
-    case "${options}" in
-        z)
-            ZIP=1
-            ;;
-        j)
-            BZ=1
-            ;;
-				f)
-						FORCE=1
-						;;
-        o)
-            OUTDIR="${OPTARG}"
-            ;;
-        :)
-            echo "${RED}Error: -${OPTARG} requires an argument.${RS}"
-            exit_abnormal
-            ;;
-        *)
-            exit_abnormal
-            ;;
-        esac
+  case "${options}" in
+    z)
+      ZIP=1
+      ;;
+    j)
+      BZ=1
+      ;;
+    f)
+      FORCE=1
+      ;;
+    o)
+      OUTDIR="${OPTARG}"
+      ;;
+    :)
+      echo "${RED}Error: -${OPTARG} requires an argument.${RS}"
+      exit_abnormal
+      ;;
+    *)
+      exit_abnormal
+      ;;
+  esac
 done
 
 # Convert OUTDIR to absolute path
 OUTDIR=$(cd "$OUTDIR"; pwd)
 
 if [[ ! -d "$OUTDIR" ]]; then
-    echo "${RED}${OUTDIR} is not a directory!${RS}"
-    exit_abnormal
+  echo "${RED}${OUTDIR} is not a directory!${RS}"
+  exit_abnormal
 fi
 
 if [[ $ZIP == 0 && $BZ == 0 ]]; then
-    echo "${RED}You must pick at least one archive option, -z / -j.${RS}"
-    exit_abnormal
+  echo "${RED}You must pick at least one archive option, -z / -j.${RS}"
+  exit_abnormal
 fi
 
 
 if [[ ! -d "${TMPDIR}" ]] ; then mkdir "${TMPDIR}"; fi
 if [ $? != 0 ] ; then
-	echo "Error creating ${TMPDIR} exiting."
-	exit 1
+  echo "Error creating ${TMPDIR} exiting."
+  exit 1
 fi
 
 TEXFILES=()
@@ -177,56 +177,56 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 for TEX in $@
 do
-    if [[ -d "${TEX}" ]]; then
-        if [[ "$OUTDIR" != $(cd "$TEX"; pwd) ]]; then
-            echo "${YELLOW}Skipping directory ${TEX}${RS}"
-        fi
-        continue
+  if [[ -d "${TEX}" ]]; then
+    if [[ "$OUTDIR" != $(cd "$TEX"; pwd) ]]; then
+      echo "${YELLOW}Skipping directory ${TEX}${RS}"
     fi
-	echo ${TEX} | grep -q \.tex
-	if [[ "$?" == 0 ]] ; then
-		TEXFILES+=($TEX)
-		echo "Finding deps for ${TEX}"
-		finddeps "${TEX}" | xargs -n 1 -I % rsync -q --relative % "${TMPDIR}"
-	    BIB=$(grep '\\bibliography' manuscript.tex | cut -d '{' -f 2 | sed 's+}+.bib+')
-        if [[ -f "$BIB" ]] ; then
-            echo "${POWDER_BLUE}Adding ${BIB}${RS}"
-            cp "${BIB}" "${TMPDIR}"
-        fi
-    elif [[ -f "${TEX}" ]]; then
-		cp "${TEX}" "${TMPDIR}"
-	fi
+    continue
+  fi
+  echo ${TEX} | grep -q \.tex
+  if [[ "$?" == 0 ]] ; then
+    TEXFILES+=($TEX)
+    echo "Finding deps for ${TEX}"
+    finddeps "${TEX}" | xargs -n 1 -I % rsync -q --relative % "${TMPDIR}"
+    BIB=$(grep '\\bibliography' manuscript.tex | cut -d '{' -f 2 | sed 's+}+.bib+')
+    if [[ -f "$BIB" ]] ; then
+      echo "${POWDER_BLUE}Adding ${BIB}${RS}"
+      cp "${BIB}" "${TMPDIR}"
+    fi
+  elif [[ -f "${TEX}" ]]; then
+    cp "${TEX}" "${TMPDIR}"
+  fi
 done
 IFS=$SAVEIFS
 
 cd "${TMPDIR}"
 if [ $? == 0 ]
 then
-	cataux # Cat any aux files required to render the tex file for portability
-	catclass  # Cat the custom class file into the tex file for portability
-	echo "${LIME_YELLOW}Removing comments from *.tex${RS}"
-	printf '%s\0' *.tex | xargs -0 -n 1 sed -i.bak '/^%.*$/d'
-	flattendirs # Get rid of graphicspath and flatten directory structure
-	checktex # Make sure the cleaned tex files are OK
-	texok=$?
-	[[ $FORCE == 1 ]] && texok=0
+  cataux # Cat any aux files required to render the tex file for portability
+  catclass  # Cat the custom class file into the tex file for portability
+  echo "${LIME_YELLOW}Removing comments from *.tex${RS}"
+  printf '%s\0' *.tex | xargs -0 -n 1 sed -i.bak '/^%.*$/d'
+  flattendirs # Get rid of graphicspath and flatten directory structure
+  checktex # Make sure the cleaned tex files are OK
+  texok=$?
+  [[ $FORCE == 1 ]] && texok=0
   # Cleanup
-	while read todel; do
-		rm "$todel"
-	done < <(cat .todel)
-	rm *.out *.bak .todel 2>/dev/null
+  while read todel; do
+    rm "$todel"
+  done < <(cat .todel)
+  rm *.out *.bak .todel 2>/dev/null
 
-    if [[ $ZIP == 1 ]] && [[ $texok == 0 ]]; then
-        echo "${POWDER_BLUE}Compressing files to ${OUTDIR}/${BASENAME}.zip${RS}"
-        zip -r9 "${OUTDIR}/${BASENAME}.zip" *
-    fi
-    if [[ $BZ == 1 ]] && [[ $texok == 0 ]]; then
-        echo "${POWDER_BLUE}Compressing files to ${OUTDIR}/${BASENAME}.tar.bz2${RS}"
-        tar -cjvf "${OUTDIR}/${BASENAME}.tar.bz2" *
-    fi
-    cd ..
-	rm -fr "${TMPDIR}"
+  if [[ $ZIP == 1 ]] && [[ $texok == 0 ]]; then
+    echo "${POWDER_BLUE}Compressing files to ${OUTDIR}/${BASENAME}.zip${RS}"
+    zip -r9 "${OUTDIR}/${BASENAME}.zip" *
+  fi
+  if [[ $BZ == 1 ]] && [[ $texok == 0 ]]; then
+    echo "${POWDER_BLUE}Compressing files to ${OUTDIR}/${BASENAME}.tar.bz2${RS}"
+    tar -cjvf "${OUTDIR}/${BASENAME}.tar.bz2" *
+  fi
+  cd ..
+  rm -fr "${TMPDIR}"
 else
-	echo "${RED}Error enterting temp dir ${TMPDIR}${RS}"
-	exit 1
+  echo "${RED}Error enterting temp dir ${TMPDIR}${RS}"
+  exit 1
 fi
